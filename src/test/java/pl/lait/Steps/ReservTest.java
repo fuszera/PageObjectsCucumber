d2bb182d1ead1e59820fddc60e428b3978768668
user.email <kgryglicka@gmail.com>
2017-06-18 22:51:33 +0200

    ReservTest has @After and @Before copied from MainPageTest, ava.lang.NullPointerException 	at pl.lait.Setup.Init.close(Init.java:28) 	at pl.lait.Setup.Init.cleanUp(Init.java:35) 	at pl.lait.Steps.ReservTest.end(ReservTest.java:21) appears, connected with @After

--- src/test/java/pl/lait/Steps/ReservTest.java
+++ src/test/java/pl/lait/Steps/ReservTest.java
@@ -1,5 +1,23 @@
 package pl.lait.Steps;
 
-public class ReservTest {
+import org.openqa.selenium.WebDriver;
 
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import pl.lait.Setup.Init;
+
+public class ReservTest extends Init{
+
+	protected WebDriver driver;
+	
+	@Before
+	public void init(){
+		driver = getDriver();
+		
+	}
+	
+	@After
+	public void end(){
+		cleanUp();
+	}
 }
